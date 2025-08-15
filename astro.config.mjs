487f06e90f798cd0dd8532bcc01395ea18d19f45
matiasgelpi <matiasjgelpi@gmail.com>
2025-08-15 12:26:16 -0300

    agrego adaptador netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,17 @@
-// @ts-check
+
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
 import keystatic from "@keystatic/astro";
 
-// https://astro.build/config
+import netlify from "@astrojs/netlify";
+
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
 
   integrations: [react(), markdoc(), keystatic()],
+  adapter: netlify(),
 });
